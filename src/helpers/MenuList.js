7f1644a857f2df47e59158df504fb96f912a9171
Lakshmi Subhash <subhashande72071@gmail.com>
2025-03-08 22:07:31 +0530

    Initial commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 399,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 349,
+  },
+  {
+    name: "Corn n Cheese Paratha Pizza",
+    image: PedroTechSpecial,
+    price: 649,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 249,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 179,
+  },
+  {
+    name: "Veg Extravaganza Pizza",
+    image: Expensive,
+    price: 999,
+  },
+];
